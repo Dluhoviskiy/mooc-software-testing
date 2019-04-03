48907d382f3f5eb59d1e64237b1c7cabc70bfa07
Dluhoviskiy <dm.luhoviskiy@gmail.com>
2019-04-03 16:26:15 +0300

    Added standard HTML page tags

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,36 +1,42 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
+    private RomanNumeral roman;
 
+    @BeforeEach
+    public void initialize() {
+        this.roman = new RomanNumeral();
+    }
 
     @Test
     public void singleNumber() {
-        RomanNumeral roman = new RomanNumeral();
+        //RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("I");
         Assertions.assertEquals(1, result);
     }
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("VIII");
-        Assertions.assertEquals(8, result);
+        //RomanNumeral roman = new RomanNumeral();
+        int result = roman.convert("VVVV");
+        Assertions.assertEquals(25, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
+        //RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
-        int result = roman.convert("XLIV");
-        Assertions.assertEquals(44, result);
+        //RomanNumeral roman = new RomanNumeral();
+        int result = roman.convert("XXX");
+        Assertions.assertEquals(30, result);
     }
 }
